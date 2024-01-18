b97514a9b58b3e473853e5b3b4b29da8c88b339e
Ali Mahdiyar <alimahdiyar77@gmail.com>
2024-01-18 17:58:11 +0330

    feat: update sample content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,15 +8,6 @@ const Home = () => {
   }
   return (
     <div>
-      <div className="border-2 border-red-500 p-5">
-        Penalties to claim: 10 GHO
-        <br />
-        <br />
-        To claim your penalties, you should create a deposit of at least 10 GHO
-        into a saving account. Your penalty amount will be added to your saving
-        deposit <br />
-      </div>
-      <br />
       <button className="mx-2 btn-primary">Create saving account</button>
       <br />
       <br />
@@ -41,6 +32,9 @@ const Home = () => {
           <div className="mx-2">2024/01/10 12:52</div>
           <button className="mx-2 btn-primary">Add funds?</button>
           <button className="mx-2 btn-primary">Withdraw</button>
+          <button className="mx-2 btn-primary">
+            Copy emergency withdraw allowance url
+          </button>
         </div>
         <div className="flex my-2">
           <div className="mx-2">200 GHO</div>
@@ -51,6 +45,9 @@ const Home = () => {
           <div className="mx-2">2024/01/10 12:52</div>
           <button className="mx-2 btn-primary">Add funds?</button>
           <button className="mx-2 btn-primary">Withdraw</button>
+          <button className="mx-2 btn-primary">
+            Copy emergency withdraw allowance url
+          </button>
         </div>
       </div>
       <hr />
@@ -63,6 +60,9 @@ const Home = () => {
           <br />
           <input type="text" placeholder="Enter deposit amount" /> GHO
           <br />
+          <input type="text" placeholder="Emergency contact address" /> Wallet
+          of someone who can let you get more money for emergency cases
+          <br />
           <button className="mx-2 btn-primary">Create saving account</button>
         </div>
       </div>
@@ -72,12 +72,21 @@ const Home = () => {
         <br />
         <div>
           <input type="text" placeholder="Amount to withdraw" /> Available
-          amount: 100 GHO. if you withdraw more than 90% of your available
-          amount, 10% of the amount to withdraw from your remaining periods will
-          be deducted as a penalty and can be claimed by you later if you
-          deposit more funds into active saving accounts
+          amount: 100 GHO.
           <br />
           <button className="mx-2 btn-primary">Withdraw</button>
+        </div>
+      </div>
+      <hr />
+      <div>
+        Emergency allowance form:
+        <br />
+        <div>
+          <input type="text" placeholder="Amount to withdraw" /> Amount of funds
+          you want the depositor to be able to withdraw more than their
+          available amount
+          <br />
+          <button className="mx-2 btn-primary">Release funds</button>
         </div>
       </div>
     </div>
