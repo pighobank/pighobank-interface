32f2af440f9b2a41d7abf90eac95b52de8416e27
Ali Mahdiyar <alimahdiyar77@gmail.com>
2024-01-20 22:27:56 +0330

    feat: add PiGHOBank smart contract abi

--- wagmi.config.ts
+++ wagmi.config.ts
@@ -1,6 +1,16 @@
-import {defineConfig} from '@wagmi/cli';
-import {react} from '@wagmi/cli/plugins';
+import { defineConfig } from "@wagmi/cli";
+import { react } from "@wagmi/cli/plugins";
+import { Abi } from "viem";
+
+import PiGHOBankABI from "./src/abis/PiGHOBank.json";
+
 export default defineConfig({
-  out: 'src/abis/types/generated.ts',
+  out: "src/abis/types/generated.ts",
+  contracts: [
+    {
+      name: "PiGHOBank",
+      abi: PiGHOBankABI as Abi,
+    },
+  ],
   plugins: [react()],
 });
