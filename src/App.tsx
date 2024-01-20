d3653081aa9c0919b8d93af350af5944ce209d3e
Ali Beigi <beigi.work@gmail.com>
2024-01-20 13:01:29 +0400

    update navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,26 @@ import React from "react";
 import Home from "pages";
 import { Route, Routes } from "react-router-dom";
 import { ConnectKitButton } from "connectkit";
+import { useAccount } from "wagmi";
 
-// function Header() {
-//   return (
-//     <header className="flex justify-between items-center flex-col sm:flex-row p-4 bg-gray-800 text-white">
-//       Pighobank <ConnectKitButton />
-//     </header>
-//   );
-// }
+function Header() {
+  const { address } = useAccount();
+  return (
+    <header className={`flex ${address ? 'justify-between' : 'justify-end'} items-center flex-col sm:flex-row p-4 text-white`}>
+      {address && <div className={'flex items-center gap-2'}>
+        <img src={'/img/piggy.png'} className={'opacity-90 w-14'}/>
+        <h1 className={'pt-2 font-medium text-3xl tracking-wide'}>PI<span className={'typog text-2xl'}>GHO</span>BANK</h1>
+      </div>}
+      <ConnectKitButton/>
+    </header>
+  );
+}
 
 function App() {
   return (
     <div className="flex flex-col min-h-screen">
-      <main className="flex-grow">
-        {/*<Header />*/}
+      <main className="flex-grow  h-screen">
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,13 +4,13 @@ import { ConnectKitButton } from "connectkit";
 
 const Home = () => {
   const { address } = useAccount();
+  // translate-y-1
   if (!address) {
-    return <div className="flex flex-col justify-center items-center h-screen">
+    return <div className="flex flex-col justify-center items-center">
       <img src={'/img/piggy.png'} className={'opacity-90 w-64'}/>
       <h1 className={'font-medium text-[84px] tracking-wide'}>PI<span className={'typog text-7xl'}>GHO</span>BANK</h1>
       <ConnectKitButton customTheme={{
         "--ck-font-family": '"Rubik", sans-serif;',
-        "--ck-font-size": '96px',
       }} />
       {/*<button type="button"*/}
       {/*        className="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Teal*/}
